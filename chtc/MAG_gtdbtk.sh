64ac847f11458455e17c53ec311ccfb199881237
jkuehn10 <121126804+jkuehn10@users.noreply.github.com>
2026-02-06 10:29:26 -0600

    Replaced MAG taxonomy classification scripts

--- chtc/MAG_gtdbtk.sh
+++ chtc/MAG_gtdbtk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # transfer conda env file
-cp /staging/qzhang333/gtdbtk-v2.3.2.tar.gz .
+cp /staging/kuehn4/MARS_WGS/gtdbtk-v2.3.2.tar.gz .
 
 # define env
 ENVNAME=gtdbtk-v2.3.2
@@ -15,7 +15,7 @@ tar -xzf $ENVNAME.tar.gz -C $ENVDIR
 
 
 # transfer and unpack gtdb-tk database
-cp /staging/qzhang333/gtdbtk_r214_data.tar.gz .
+cp /staging/groups/rey_group_datashare/gtdbtk_r214_data.tar.gz .
 tar -xzf gtdbtk_r214_data.tar.gz
 rm gtdbtk_r214_data.tar.gz
 
@@ -23,18 +23,18 @@ rm gtdbtk_r214_data.tar.gz
 export GTDBTK_DATA_PATH=$(pwd)/release214/
 
 # transfer bin fa file
-cp /staging/qzhang333/DOFS_MAG_NR.tar.gz .
-tar -zxf DOFS_MAG_NR.tar.gz
+cp /staging/kuehn4/MARS_WGS/MARS_MAG_NR.tar.gz .
+tar -zxf MARS_MAG_NR.tar.gz
 
 # create dir of output
 mkdir MAG_gtdbtk_out
 
 # running GTDB-Tk
-gtdbtk classify_wf --extension fa --genome_dir DOFS_MAG_NR/ --out_dir MAG_gtdbtk_out --cpus 4 --skip_ani_screen
+gtdbtk classify_wf --extension fa --genome_dir MARS_MAG_NR/ --out_dir MAG_gtdbtk_out --cpus 4 --skip_ani_screen
 
-# move output to staging/qzhang333
+# move output to staging/kuehn4
 tar -czf MAG_gtdbtk_out.tar.gz MAG_gtdbtk_out
-mv MAG_gtdbtk_out.tar.gz /staging/qzhang333
+mv MAG_gtdbtk_out.tar.gz /staging/kuehn4/MARS_WGS
 
 # clear other data
 rm *.tar.gz
