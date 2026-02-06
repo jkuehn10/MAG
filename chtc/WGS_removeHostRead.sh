fd05a6075eeaaa3d3211c0c17887aea51466e843
jkuehn10 <121126804+jkuehn10@users.noreply.github.com>
2026-02-06 14:31:17 -0600

    Replaced scripts to remove host reads

--- chtc/WGS_removeHostRead.sh
+++ chtc/WGS_removeHostRead.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # unpack pipeline 
 unzip bowtie2-2.3.4-linux-x86_64.zip
 tar -xzf samtools-1.19.tar.gz
@@ -17,47 +16,47 @@ export PATH=$(pwd)/python-3.8/bin:$PATH
 export PYTHONPATH=$(pwd)/python-3.8-packages
 
 
-# transfer trimmed reads and indexed mouse genome
-cp /staging/qzhang333/DOdiet_WGS/$1_R1_trimmomaticTrimmed_paired.fastq.gz .  
-cp /staging/qzhang333/DOdiet_WGS/$1_R2_trimmomaticTrimmed_paired.fastq.gz . 
-cp /staging/qzhang333/Mus_musculus_GRCm38_Rel98_bowtie2_index.tar.gz .
+# transfer trimmed reads and indexed human genome
+cp /staging/kuehn4/MARS_WGS/$1_R1_trimmomaticTrimmed_paired.fastq.gz .  
+cp /staging/kuehn4/MARS_WGS/$1_R2_trimmomaticTrimmed_paired.fastq.gz . 
+cp /staging/kuehn4/PROG_instl/Homo_sapiens_GRCh38_bowtie2_index.tar.gz .
 
 # uncompress
 gunzip $1_R1_trimmomaticTrimmed_paired.fastq.gz
 gunzip $1_R2_trimmomaticTrimmed_paired.fastq.gz
-tar -xzf Mus_musculus_GRCm38_Rel98_bowtie2_index.tar.gz
+tar -xzf Homo_sapiens_GRCh38_bowtie2_index.tar.gz
 
-# alignment WGS reads to mouse genome
-bowtie2 -x Mus_musculus_GRCm38_Rel98_bowtie2_index/ref \
+# alignment WGS reads to human genome
+bowtie2 -x Homo_sapiens_GRCh38_bowtie2_index/ref \
         -1 $1_R1_trimmomaticTrimmed_paired.fastq \
         -2 $1_R2_trimmomaticTrimmed_paired.fastq \
-        --local -p 1 |
-samtools view -bS > $1_mouseGenome_align.bam
+        -p 4 |
+samtools view -bS > $1_humanGenome_align.bam
 
-# get mouse genome unmapped sam file
-samtools view -b -f 4 -f 8 -o $1_mouseDNA_unmapped.bam $1_mouseGenome_align.bam
+# get human genome unmapped sam file
+samtools view -b -f 4 -f 8 -o $1_humanDNA_unmapped.bam $1_humanGenome_align.bam
 
 # sort unmapped bam file
-samtools sort -n $1_mouseDNA_unmapped.bam -o $1_mouseDNA_unmapped_sorted.bam
-rm $1_mouseDNA_unmapped.bam
+samtools sort -n $1_humanDNA_unmapped.bam -o $1_humanDNA_unmapped_sorted.bam
+rm $1_humanDNA_unmapped.bam
 
 >&2 echo "flagstat of sorted bam file: "
->&2 samtools flagstat $1_mouseDNA_unmapped_sorted.bam
+>&2 samtools flagstat $1_humanDNA_unmapped_sorted.bam
 
 # extract unmapped reads from bam file
-bamToFastq -i $1_mouseDNA_unmapped_sorted.bam \
-           -fq $1_R1_trimmed_paired_mouseDNAremoved.fastq \
-           -fq2 $1_R2_trimmed_paired_mouseDNAremoved.fastq
+bamToFastq -i $1_humanDNA_unmapped_sorted.bam \
+           -fq $1_R1_trimmed_paired_humanDNAremoved.fastq \
+           -fq2 $1_R2_trimmed_paired_humanDNAremoved.fastq
 
 # compress fastq data
-gzip -f $1_R1_trimmed_paired_mouseDNAremoved.fastq
-gzip -f $1_R2_trimmed_paired_mouseDNAremoved.fastq
+gzip -f $1_R1_trimmed_paired_humanDNAremoved.fastq
+gzip -f $1_R2_trimmed_paired_humanDNAremoved.fastq
 
 # transfer reconstruct unmapped and mapped reads into gluster
-mv $1_R1_trimmed_paired_mouseDNAremoved.fastq.gz /staging/qzhang333/DOdiet_WGS/
-mv $1_R2_trimmed_paired_mouseDNAremoved.fastq.gz /staging/qzhang333/DOdiet_WGS/
+mv $1_R1_trimmed_paired_humanDNAremoved.fastq.gz /staging/kuehn4/MARS_WGS/
+mv $1_R2_trimmed_paired_humanDNAremoved.fastq.gz /staging/kuehn4/MARS_WGS/
 
-# clear data
-rm *.tar.gz
-rm *.fastq
+# clear space
 rm *.bam
+rm *.fastq
+rm *.tar.gz
